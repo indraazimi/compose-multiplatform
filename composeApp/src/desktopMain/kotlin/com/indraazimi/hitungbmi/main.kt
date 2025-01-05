c84e81c92c5a7f7edb00ae67fdd635b381568b72
Indra Azimi <indraazimi@yahoo.com>
2025-01-05 10:53:59 +0700

    Initial commit

---
+++ composeApp/src/desktopMain/kotlin/com/indraazimi/hitungbmi/main.kt
@@ -0,0 +1,13 @@
+package com.indraazimi.hitungbmi
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Hitung BMI",
+    ) {
+        App()
+    }
+}
